50b6305af712e46eba06c9d268203601d8d739a0
bacng <nhb.fit.hut@gmail.com>
2022-11-22 16:41:08 +0700

    Folder structure of project

--- src/App.js
+++ src/App.js
@@ -2,21 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const name = 'Bac'
+  const age = 20;
+  const isFemale = false;
+  const student = {
+    name: 'Backend'
+  }
+  const colorList = ['red', 'green', 'blue']
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bac Ng
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Xin chao {name} - {age} - {isFemale ? 'Male' : 'Female'}</p>
+        {isFemale ? <p>Male</p> : <p>Female</p>}
+        <p>{student.name}</p>
+
+        <ul>
+          {colorList.map(color => (
+            <li key={color} style={{ color }}>{color}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
