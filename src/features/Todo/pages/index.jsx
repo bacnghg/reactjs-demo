a841482fb370a53b528d0ed63d53b2b726b9c470
bacng <nhb.fit.hut@gmail.com>
2022-11-24 15:15:11 +0700

    using state for Filter

--- src/features/Todo/pages/index.jsx
+++ src/features/Todo/pages/index.jsx
@@ -26,6 +26,7 @@ function TodoFeature(props){
     ];
 
     const [todoList, setTodoList] = useState(initTodoList)
+    const [filteredStatus, setFilteredStatus] = useState('all')
     
     const handleTodoClick = (todo, idx) => {
         console.log(todo, idx);
@@ -43,11 +44,32 @@ function TodoFeature(props){
         setTodoList(newTodoList);
     }
 
+    const handleShowAllClick = () => {
+        setFilteredStatus('all');
+    }
+
+    const handleShowCompletedClick = () => {
+        setFilteredStatus('completed')
+    }
+
+    const handleShowNewClick = () => {
+        setFilteredStatus('new')
+    }
+
+    const renderTodoList = todoList.filter(todo => filteredStatus === 'all' || filteredStatus === todo.status)
+    // console.log(renderTodoList)
+
     return (
         <div>
             <h3>Todo List</h3>
-            <TodoList todoList={todoList} onTodoClick={handleTodoClick}/>
+            <TodoList todoList={renderTodoList} onTodoClick={handleTodoClick}/>
+            <div>
+                <button onClick={handleShowAllClick}>Show All</button>
+                <button onClick={handleShowCompletedClick}>Show Completed</button>
+                <button onClick={handleShowNewClick}>Show New</button>
+            </div>
         </div>
+        
     );
 }
 
